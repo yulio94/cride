658d3e7398a94f452855920e6ac01d692cb3abee
yulio94 <me@julioestrada.dev>
2021-01-16 00:11:43 -0600

    feat: permission to validate if a person is an active member of a circle.

--- cride/circles/permissions/__init__.py
+++ cride/circles/permissions/__init__.py
@@ -1,1 +1,3 @@
-from cride.circles.permissions.is_circle_admin import IsCircleAdmin
+from cride.circles.permissions.circles import IsCircleAdmin
+from cride.circles.permissions.memberships import IsActiveCircleMember
+from cride.circles.permissions.invitations import IsSelfMember
