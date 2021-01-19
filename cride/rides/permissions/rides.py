944a20aa389bf7bd12ce3cd9d137883b5c16ed12
yulio94 <me@julioestrada.dev>
2021-01-18 23:05:28 -0600

    feat: created permission to validate is request user is the owner of a ride or not.

---
+++ cride/rides/permissions/rides.py
@@ -0,0 +1,20 @@
+"""Rides permissions."""
+
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+
+class IsRideOwner(BasePermission):
+    """Verify requesting user is the ride creator."""
+
+    def has_object_permission(self, request, view, obj):
+        """Verify requesting user is the ride creator."""
+        return request.user == obj.offered_by
+
+
+class IsNotRideOwner(BasePermission):
+    """Verify requesting user isn't the ride creator."""
+
+    def has_object_permission(self, request, view, obj):
+        """Verify requesting user isn't the ride creator."""
+        return request.user != obj.offered_by
